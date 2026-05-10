b9351fd67f894dea577bf7863d1f6953e98bc2c1
Kiwinrar <kumarsaksham2331@gmail.com>
2026-05-11 04:23:54 +0530

    Added basic enviroment command extraction logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    let parameter = ConfigQuery::new(&arguments);
+    match parameter {
+        Ok(value) => {
+            value.query;
+            let contents_of_file = fs::read_to_string(value.filename);
+            match contents_of_file {
+                Ok(contents) => println!("{}", contents),
+                Err(er) => {
+                    println!("Error reading the contents of the file:\n{}", er);
+                    process::exit(2)
+                }
+            }
+        }
+        Err(err) => {
+            println!("Error parsing the parameters: {}", err);
+            process::exit(1)
+        }
+    }
+}
+
+struct ConfigQuery<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+impl<'a> ConfigQuery<'a> {
+    fn new(args: &'a Vec<String>) -> Result<ConfigQuery<'a>, &'a str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query: &str = &args[1];
+        let filename: &str = &args[2];
+        print!("{}", filename);
+        let parameter = ConfigQuery {
+            query,
+            filename,
+        };
+        Ok(parameter)
+    }
 }
