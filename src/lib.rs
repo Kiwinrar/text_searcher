c60b0f9977775fa51b75ccd4106f2026407d5af5
Kiwinrar <kumarsaksham2331@gmail.com>
2026-05-12 03:14:11 +0530

    Streamlined the overall complexity of the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use std::error::Error;
 use std::fs;
 
-pub fn run(config_parameter: ConfigQuery, search_parameter: &str) -> Result<(), Box<dyn Error>> {
+pub fn run(
+    config_parameter: ConfigQuery,
+    search_parameter: &[String]
+) -> Result<(), Box<dyn Error>> {
     let contents_of_file = fs::read_to_string(config_parameter.filename)?;
     if matches!(config_parameter.query, Commands::Search) {
         let search_result = search(search_parameter, &contents_of_file);
-        for val in search_result.iter(){
+        for val in search_result.iter() {
             println!("{}", val);
         }
     }
@@ -14,15 +17,12 @@ pub fn run(config_parameter: ConfigQuery, search_parameter: &str) -> Result<(), 
     }
     Ok(())
 }
-pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(query: &'a [String], contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    for (i, ch) in query.chars().enumerate() {
-        if ch == ' ' {
-            let str = &query[i+1..query.len()];
-            for line in contents.lines() {
-                if line.contains(str) {
-                    results.push(line);
-                }
+    for (_i, ch) in query.iter().enumerate() {
+        for line in contents.lines(){
+            if line.contains(ch){
+                results.push(line);
             }
         }
     }
@@ -38,39 +38,56 @@ pub enum Commands {
     Search,
 }
 impl<'a> ConfigQuery<'a> {
-    pub fn new(args: &'a Vec<String>) -> Result<(ConfigQuery<'a>, String), &'a str> {
+    pub fn new(args: &'a Vec<String>) -> Result<(&'a [String], &'a String), &'a str> {
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
-        let query: &str = &args[1];
-        let command_query = match query {
-            "read" => Commands::Read,
-            "search" => Commands::Search,
-            &_ => {
-                return Err("Invalid query");
+        let mut file_index = None;
+        for (i, val) in args.iter().enumerate() {
+            if i == 0 {
+                continue;
             }
-        };
-        let word;
-        let filename: &String;
-        let search_query;
-        match command_query {
-            Commands::Read => {
-                filename = &args[2];
-                search_query = "".to_string();
-            }
-            Commands::Search => {
-                if args.len() < 4 {
-                    return Err("The search argument is not provided");
-                }
-                word = &args[2];
-                search_query = format!("{} {}", query, word);
-                filename = &args[3];
+            if val.contains(".txt") {
+                file_index = Some(i);
+                break;
             }
         }
-        let parameter = ConfigQuery {
-            query: command_query,
-            filename: filename,
+        let index = match file_index {
+            Some(i) => i,
+            None => {
+                return Err("File name missing");
+            }
         };
-        Ok((parameter, search_query))
+        let query = &args[1..index];
+        let filename = &args[index];
+
+        Ok((query, filename))
+    }
+
+    pub fn parameters(
+        queries: &'a [String],
+        filename: &'a String
+    ) -> Result<(ConfigQuery<'a>, &'a [String]), &'a str> {
+        let command_query = &queries[0];
+        
+        let config_command = match command_query.as_str() {
+            "read" => Commands::Read,
+            "search" => Commands::Search,
+            _ => {
+                return Err("Invalid Command query");
+            }
+        };
+        let query_parameter = &queries[1..queries.len()];
+        
+        if query_parameter.is_empty() {
+            if matches!(config_command, Commands::Search) {
+                return Err("The arguments to the query are missing");
+            }
+        }
+        let config_parameter = ConfigQuery {
+            query: config_command,
+            filename,
+        };
+        Ok((config_parameter, query_parameter))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,25 @@ use std::process;
 use text_searcher;
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    let parameters = match text_searcher::ConfigQuery::new(&arguments) {
-        Ok(value) => {
-            let (val, search_parameter) = value;
-            println!("The given query: {:?}", val.query);
-            if search_parameter != "".to_string() {
-                println!("Search parameter: {}", search_parameter);
-            }
-            (val,search_parameter)
-        }
-        Err(e) => {
-            println!("Error parsing the arguments: {}", e);
-            process::exit(1)
+    let parameters=match text_searcher::ConfigQuery::new(&arguments){
+        Ok(queries)=>{
+            queries
+        },
+        Err(e)=>{println!("Error parsing arguments: {}", e);process::exit(1)}
+    };
+    let (queries, filename)=parameters;
+    let query_output=match text_searcher::ConfigQuery::parameters(queries, filename){
+        Ok(query)=>{
+            let (config_query, queries_array)=query;
+            (config_query, queries_array)
+        },
+        Err(e)=>{
+            println!("Error satifying the query: {}", e);
+            process::exit(2);
         }
     };
-    let (val, search_parameter)=parameters;
-    if let Err(e) = text_searcher::run(val, &search_parameter) {
+    let (val, search_parameter)=query_output;
+    if let Err(e) = text_searcher::run(val, search_parameter) {
         println!("Application Error: {}", e);
         process::exit(2)
     }
