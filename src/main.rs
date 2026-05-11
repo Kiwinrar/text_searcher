8427a72775d24fa9f8f26bf4f330f13065a09c6f
Kiwinrar <kumarsaksham2331@gmail.com>
2026-05-12 03:18:53 +0530

    Streamlined the variable names

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,16 @@ fn main() {
     let (queries, filename)=parameters;
     let query_output=match text_searcher::ConfigQuery::parameters(queries, filename){
         Ok(query)=>{
-            let (config_query, queries_array)=query;
-            (config_query, queries_array)
+            let (config_query, query_parameter)=query;
+            (config_query, query_parameter)
         },
         Err(e)=>{
             println!("Error satifying the query: {}", e);
             process::exit(2);
         }
     };
-    let (val, search_parameter)=query_output;
-    if let Err(e) = text_searcher::run(val, search_parameter) {
+    let (config_query, query_parameter)=query_output;
+    if let Err(e) = text_searcher::run(config_query, query_parameter) {
         println!("Application Error: {}", e);
         process::exit(2)
     }
